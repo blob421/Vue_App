4e7fd8df163fa845cc8826a8ccdc041e7b897776
Gabriel Blanchet-Poitras <gabrielbpoitras@gmail.com>
2025-07-13 23:20:46 -0400

    Added a second graph for Eth. Made the plotly graphs static , reduced the title's width .

--- src/components/graph.vue
+++ src/components/graph.vue
@@ -3,6 +3,7 @@
        
       <h class="h">Welcome to my web</h>
       <div ref="plotlyChart" class="graph"></div>
+      <div ref = "plotlyChart2" class = "graph2"></div>
     </div>
    
 </template>
@@ -12,11 +13,13 @@ import Plotly from 'plotly.js-dist';
 import { ref, watch} from "vue";
 
 const plotlyChart = ref(null);
-const { dates, prices } = defineProps(["dates", "prices"]);
+const plotlyChart2 = ref(null);
+const { dates, prices, dates2, prices2 } = defineProps(["dates", "prices", "dates2", "prices2"]);
 
-watch([() => dates, () => prices], ([newDates, newPrices]) => {
+watch([() => dates, () => prices, () => dates2, () => prices2], () => {
 
   renderPlot();
+  renderPlot2();
 });
 
 const renderPlot = () => {
@@ -107,13 +110,112 @@ const renderPlot = () => {
           ticksuffix:"   "
       }
     };
-        const config = {responsive: true};
+        const config = {
+          responsive: true, 
+          staticPlot: true
+        };
 
      
         Plotly.newPlot(plotlyChart.value, [baseTrace, trace], layout, config);
 
   }
-  
+  const renderPlot2 = () => {
+  if (!plotlyChart2.value) {
+    console.error("Plotly container not found!");
+    return;
+  }
+      const trace2 = {
+
+        x: dates2,
+        y: prices2,
+        mode: 'lines',
+        fill: "tonexty",
+        fillgradient: {
+        type: 'vertical',
+        colorscale: [[0,'rgba(161, 255, 238, 0.3)'], [1,'rgba(0, 218, 139, 1)']],
+      },
+        showlegend: false,
+        line: {
+        color: "lightgreen",  // Change line color here
+        width: 2.7,       // Adjust thickness if needed
+       
+        
+      }
+    };
+        const minY = Math.min(...prices2);
+
+        const baseTrace2 = {
+          x: dates2,
+          y: Array(prices2.length).fill(minY),
+          type: "scatter",
+          mode: "lines",
+          line: { color: "transparent" },
+          showlegend: false,
+ 
+    };
+
+        const layout2 = { margin: {
+          r:70
+      },
+ 
+
+        title: {
+          text: "Ethereum movement",  'x': 0.5, 
+          'y':0.89, font : { size: 18, color: "#ffffff", family: "solidity: 0.2" }
+      },
+        template: "plotly_dark", // Dark theme
+        paper_bgcolor: "#171717",  // Background outside the plot
+        plot_bgcolor: '#171717',   // Background inside the plot
+        
+        xaxis: {
+          showline: false, // ✅ Removes the x-axis line
+          showgrid: false, // ✅ Removes the x-axis grid lines
+          zeroline: false, // ✅ Removes the baseline
+          title: {
+          text: "Date",
+          font: { size: 15, color: "#ffffff", family: "solidity: 0.2" }
+        },
+
+        tickangle: 0,
+        automargin: true, // ✅ Prevent labels from overlapping
+        tickmode: "auto",
+        dtick: 86400000 * 2,
+        tickformat: "%b %d",
+        linecolor: "#506784",
+        zerolinecolor: "#283442",
+        zerolinewidth: 2,
+        ticks: "",
+        color: "#171717", // ✅ Axis line color
+        tickfont: { size: 11, color: "#ffffff", family: "solidity: 0.2" }
+     
+    
+        },
+        yaxis: {
+          title: {
+            text: "USD",
+            font: { size: 14, color: "#ffffff", family: "solidity: 0.2" },
+            standoff: 15
+          },
+
+          showline: false, // ✅ Removes the x-axis line
+          zeroline: false, // ✅ Removes the baseline
+          linecolor: "#506784",
+          gridcolor: "#506784",
+          zerolinecolor: "#283442",
+          zerolinewidth: 2,
+          tickfont: { size: 11, color: "#ffffff", family: "solidity: 0.2" },
+          ticksuffix:"   "
+      }
+    };
+        const config2 = {
+          responsive: true,
+          staticPlot: true
+        };
+
+     
+        Plotly.newPlot(plotlyChart2.value, [baseTrace2, trace2], layout2, config2);
+
+  }
   
 </script>
 
@@ -128,25 +230,54 @@ const renderPlot = () => {
     justify-content: center;
 }
     body{
-        background: linear-gradient(145deg, #afcae5, #f7f7f7);
-
+    
+    
     display: flex;
     justify-content: center;
     align-items: center; /* changed from center */
     width: 100%;
     height: 100vh;
+
 }
+
+ .graph{
+   
+    margin-left: auto;
+    margin-right: auto;
+    opacity: 92%;
+    min-height: 500px;
+    /*border: 2px blue solid;*/
+   
+  }
+
+ .graph2{
+    margin-left: auto;
+    margin-right: auto;
+   
+    min-height: 500px;
+  
+    /*border: 2px green solid;*/
+    opacity: 92%;
+    padding-top: 4rem;
+  
+    
+    
+    
+ }
+
 .container{
-    min-width: 650px;
+    padding-top: 7rem;
+    min-width: 750px;
     max-width: 800px;
     position: absolute;
     top: 0%;
     left: 50%;
     transform: translateX(-50%);
     width:75%;
-    padding-top: 10rem;
+     opacity: 92%;
     /*border: 2px red solid;*/
-    max-height: 100vh;
+    
+    height: 125% !important;
     
    
 
@@ -171,50 +302,41 @@ const renderPlot = () => {
   inset 0 0 8px rgb(255, 255, 255), 
   0 4px 12px rgb(255, 255, 255);
   position: absolute;
-  top: 5%;
+  top: 1%;
   left: 50%;
-  width: 60%;
+  width: 55%;
   text-align: center;
   transform: translateX(-50%);
   opacity: 95%;}
   
-  
-  
- .graph{
-   
-    margin-left: auto;
-    margin-right: auto;
-    opacity: 92%;
-   
-  }
-@media (max-width:4000px){
+ @media (max-width:4000px){
   .container{
     top: 0%;
     transform: translateX(-50%) scale(1.75);
     transform-origin: top;
     padding-top: clamp(10rem, 20vh, 20rem);
 
-    max-height: 100%;
+   
   }
-}
+} 
+  
 @media (max-width:1500px){
   .container{
-    top: 0%;
-    transform: translateX(-50%) scale(1);
+ 
+    transform: translateX(-50%) scale(0.95);
     transform-origin: top;
-    max-height: 100%;
+   
      padding-top: 8rem;
   }
   
   
 }
 
-
 @media (max-width:767px){
   .container{
-    transform: translateX(-50%) scale(0.55);
+    transform: translateX(-50%) scale(0.5);
     transform-origin: top;
-     padding-top: clamp(10rem, 45vh, 20rem);
+     padding-top: clamp(10rem, 25vh, 20rem);
     
   }
   .h{
@@ -223,6 +345,11 @@ const renderPlot = () => {
   }
 }
 
-
+@media (max-width:450px){
+  .container{
+     transform: translateX(-50%) scale(0.45);
+    transform-origin: top;
+  }
+}
 
 </style>
--- src/views/main.vue
+++ src/views/main.vue
@@ -2,7 +2,6 @@
 import { RouterLink, RouterView } from 'vue-router'
 
 import graph from '@/components/graph.vue'
-
 import { ref, onMounted, watch } from "vue";
 import axios from "axios";
 
@@ -12,29 +11,40 @@ const stats = ref([]);
 const dates = ref([]);
 const prices = ref([]);
 
+const stats2 = ref([]);
+const dates2 = ref([]);
+const prices2 = ref([]);
+
 
 onMounted(async () => {
  
    try {
     const response = await axios.get(`${api}/api/bitcoin`);
     stats.value = response.data.content.sort((a, b) => new Date(a.date) - new Date(b.date));
-  
+
+    const response2 = await axios.get(`${api}/api/eth`);
+    stats2.value = response2.data.content.sort((a, b) => new Date(a.date) - new Date(b.date));
 
     if (Array.isArray(stats.value)) {
     dates.value = stats.value.map(entry => new Date(entry.date));
     prices.value = stats.value.map(entry => entry.price);
+  }
+    if (Array.isArray(stats2.value)) {
+    dates2.value = stats2.value.map(entry => new Date(entry.date));
+    prices2.value = stats2.value.map(entry => entry.price);
   }
    
     
   } catch (error) {
     console.error('API Error:', error);
   }
+   }
+);
 
+watch([dates, prices, dates2, prices2], () => {
 });
 
-watch([dates, prices], ([newDates, newPrices]) => {
 
-});
 </script>
 
 <template>
@@ -43,9 +53,8 @@ watch([dates, prices], ([newDates, newPrices]) => {
    
     </head>
 
-      <graph :dates="dates" :prices="prices" class="graph"/>
-       
-
+    <graph :dates="dates" :prices="prices" :dates2="dates2" :prices2="prices2"/>
+    
     <RouterView />
 
 </template>
@@ -55,6 +64,11 @@ watch([dates, prices], ([newDates, newPrices]) => {
 <style>
 body{
    background: linear-gradient(145deg, #afcae5, #f7f7f7);
-
+   
+  height: 150vh;
+  overflow-y: auto;
 }
+   
+
+
 </style>
